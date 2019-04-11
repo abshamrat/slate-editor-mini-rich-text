a975fa71c328eda5544df7b8caafdda1870919fa
shamrat <shamratakbar17@gmail.com>
2019-04-11 13:20:26 +0600

    fixed file download && deletion issue

--- src/components/file/fileThumbnail.js
+++ src/components/file/fileThumbnail.js
@@ -5,14 +5,17 @@ import txtThumb from '../../../public/assets/img/txt-thumbnails.png';
 
 function fileThumbnails(props) {
     return (
-        <div className="file-thumbnail">
-            <span className="file-thumbnail-delete">X</span>
-            <div data-url={props.url} data-name={props.name} onClick={props.onFileClick} selected={props.selected}>
+        <div className="file-thumbnail" selected={props.selected} {...props.attributes}>
+            <span onClick={props.onDeleteClick} data-file-key={props.attributes['data-key']} className="file-thumbnail-delete material-icons">cancel</span>
+            <div className="file-thumbnail-container">
                 <img className="file-thumbnail-image" src={props.isPdf ? pdfThumb : txtThumb} />
                 <a>
                     {props.name}
                     {props.children}
                 </a>
+                <div className="file-thumbnail-overly">
+                    <span data-url={props.url} data-name={props.name} onClick={props.onFileClick} className="material-icons">archive</span>
+                </div>
             </div>
         </div>
     )
